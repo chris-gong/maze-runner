e12bfe8372a7c9f12764214755b35e4944ad9a34
kcolyhs <bchen919@gmail.com>
2018-09-27 16:06:25 -0400

    Core loop (need to test this I coded this during a lecture)

--- MazeRunner.py
+++ MazeRunner.py
@@ -6,9 +6,32 @@ class MazeRunner():
 		pass
 
 	def DFS(self):
+		goal = (self.maze.dim-1,self.maze.dim-1)
 		solution = []
 		closed = []
 		fringe = StackFringe()
 		fringe.push((0,0))
+		move_vectors=[(0,1),(-1,0),(0,-1),(1,0)]
 		while not fringe.isEmpty():
-			currentCoord = fringe.
+			cur_loc = fringe.pop()
+			if cur_loc == goal:
+				# Found solution
+				pass
+			for move in move_vectors:
+				new_loc = tuple(sum(x) for x in zip(cur_loc,move))
+				if isInMaze(new_loc) and new_loc in closed:
+					fringe.push(new_loc)
+			closed.append(cur_loc)
+
+
+	def isInMaze(self,loc)
+		x = loc[0]
+		y = loc[1]
+		if x < 0 or x >= self.maze.dim:
+			return false
+		else if y < 0 or y >= self.maze.dim
+			return false
+		else:
+			return true
+
+
