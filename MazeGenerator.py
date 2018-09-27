7417b3c5416b476cee8bf80bda39c29c2664596b
kcolyhs <bchen919@gmail.com>
2018-09-27 19:37:46 -0400

    Reorganized the maze generation

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -6,7 +6,10 @@ import numpy as np
 class MazeGenerator():
 	"""MazeRunner"""
 	def __init__(self, dim):
-		self.arg = arg
+		self.dim = dim
+	def GenerateMaze(self,prob):
+		new_maze = Maze(self.dim,prob)
+		return new_maze
 
 class Maze():
 	"""MAAAZZE"""
@@ -14,16 +17,14 @@ class Maze():
 		self.grid = np.random.choice(a=[True, False], size=(dim, dim), p=[prob, 1-prob])
 		self.grid[0][0] = False
 		self.grid[dim-1][dim-1] = False
-
-
+		
+	def RenderMaze(self):
 		cmap = colors.ListedColormap(['white','black'])
 		plt.imshow(self.grid, cmap='Greys',  interpolation='nearest')
 		plt.show()
 
-
-	
-def isBlocked(p):
-	return (random.random()<p)
 		
 if __name__ == '__main__':
-	mymaze = Maze(50,1)
+	dimension = int(input("What is the demnsion of the maze?\n"))
+	percent = float(input("What is the percentage a tile is blocked?\n"))
+	mymaze = Maze(dimension,percent)
