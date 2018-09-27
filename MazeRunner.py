e42c79b63881b2fa93043256be2fcbc7fc0489cc
kcolyhs <bchen919@gmail.com>
2018-09-27 15:34:18 -0400

    Started writing DFS

--- MazeRunner.py
+++ MazeRunner.py
@@ -1,7 +1,14 @@
 import MazeGenerator
+import Fringe
 
 class MazeRunner():
 	def __init__(self):
 		pass
 
-	
+	def DFS(self):
+		solution = []
+		closed = []
+		fringe = StackFringe()
+		fringe.push((0,0))
+		while not fringe.isEmpty():
+			currentCoord = fringe.
