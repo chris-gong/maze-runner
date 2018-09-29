71fcb5197276be05d91f8e75174a55a5d13ba3c2
Bailey Chen <bchen919@gmail.com>
2018-09-29 14:05:03 -0400

    Standarized the tabbing to four spaces

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -1,33 +1,30 @@
-import random
 import matplotlib.pyplot as plt
-from matplotlib import colors
 import numpy as np
 
 class MazeGenerator():
-	"""MazeRunner"""
-	def __init__(self, dim):
-		self.dim = dim
+    """MazeRunner"""
+    def __init__(self, dim):
+        self.dim = dim
 
-	def GenerateMaze(self,prob):
-		new_maze = Maze(self.dim,prob)
-		return new_maze
+    def GenerateMaze(self,prob):
+        new_maze = Maze(self.dim,prob)
+        return new_maze
 
 class Maze():
-	"""MAAAZZE"""
-	def __init__(self,dim,prob):
-		self.grid = np.random.choice(a=[True, False], size=(dim, dim), p=[prob, 1-prob])
-		self.grid[0][0] = False
-		self.grid[dim-1][dim-1] = False
-		self.dim = dim
+    """MAAAZZE"""
+    def __init__(self,dim,prob):
+        self.grid = np.random.choice(a=[True, False], size=(dim, dim), p=[prob, 1-prob])
+        self.grid[0][0] = False
+        self.grid[dim-1][dim-1] = False
+        self.dim = dim
 
-	def RenderMaze(self):
-		cmap = colors.ListedColormap(['white','black'])
-		plt.imshow(self.grid, cmap='Greys',  interpolation='nearest')
-		plt.show()
+    def RenderMaze(self):
+        plt.imshow(self.grid, cmap='Greys',  interpolation='nearest')
+        plt.show()
 
-		
+        
 if __name__ == '__main__':
-	dimension = int(input("What is the demnsion of the maze?\n"))
-	percent = float(input("What is the percentage a tile is blocked?\n"))
-	mymaze = Maze(dimension,percent)
-	mymaze.RenderMaze()
+    dimension = int(input("What is the demnsion of the maze?\n"))
+    percent = float(input("What is the percentage a tile is blocked?\n"))
+    mymaze = Maze(dimension,percent)
+    mymaze.RenderMaze()
