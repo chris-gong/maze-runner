554f7f7933c713c98c900800ac5c675aac002366
kcolyhs <bchen919@gmail.com>
2018-09-30 00:27:48 -0400

    Added man/euc dist func

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import sqrt,fabs
 
 class MazeGenerator():
     """MazeRunner"""
@@ -21,10 +22,21 @@ class Maze():
     def render_maze(self):
         plt.imshow(self.grid, cmap='Greys',  interpolation='nearest')
         plt.show()
-
+    
+    def euclid_dist(self,loc):
+        a = (self.dim-1-loc[0])**2
+        b = (self.dim-1-loc[1])**2
+        return sqrt(a+b)
+    
+    def manhatten_dist(self,loc):
+        a = fabs(self.dim-1-loc[0])
+        b = fabs(self.dim-1-loc[1])
+        return a + b
         
 if __name__ == '__main__':
     dimension = int(input("What is the demnsion of the maze?\n"))
     percent = float(input("What is the percentage a tile is blocked?\n"))
     mymaze = Maze(dimension,percent)
+    print(mymaze.manhat_dist100
+          ((2,3)))
     mymaze.render_maze()
