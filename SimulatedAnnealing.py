d9d5a572f88fa8359b6e7e6537560f74116355b3
kcolyhs <bchen919@gmail.com>
2018-10-06 21:06:00 -0400

    Generate Neighbors

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -1,14 +1,19 @@
 import MazeRunner
 import random
+from copy import deepcopy
 
 class SimulatedAnnealing():
     
     def __init__(self,dim,prob,search_function,*args):
-        self.runner = MazeRunner(dim,prob)
-        solution,nodes_expanded = maze_runner.search_function(*args)
+        runner = MazeRunner(dim,prob)
         self.original_maze = self.maze_runner.maze
+        solution = None
         while(solution is not None):
-            solution = maze_runner.search_function(*args)
+            solution,nodes_expanded = runner.search_function(*args)
+            runner.generate_maze()
+        
+            
+            
             
         
         
@@ -19,5 +24,22 @@ class SimulatedAnnealing():
         neighbors = []
         dim = self.runner.dim
         row_index = random.randint(0,dim-1)
-        for x in range(0,dim):
-            if maze.grid[row_index][x]
+        for x1 in range(0,dim):
+            if maze.grid[row_index][x1]:
+                for x2 in range(0,dim):
+                    if maze.grid[row_index][x2]:
+                        continue
+                    else:
+                        if((row_index == 0 and x2 == 0) or
+                            (row_index == dim-1 and x2 == dim-1)):
+                            continue
+                        new_maze = deepcopy(maze)
+                        new_maze.grid[row_index][x2] = True
+                        new_maze.grid[row_index][x1] = False
+                        neighbors.append[new_maze]
+            else:
+                pass
+        if neighbors is []:
+            return None
+        else:
+            return neighbors
